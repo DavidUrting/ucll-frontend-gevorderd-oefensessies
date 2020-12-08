b5b70e831e692354c2fc7150410b7b6c207dd191
David Urting <david.urting@live.com>
2020-12-08 22:49:17 +0100

    willekeurg kaarten leggen (zonder boundaries: dus één kaar kan meer dan 2 keer).

--- MemoryGame.Web/wwwroot/js/memory-game.js
+++ MemoryGame.Web/wwwroot/js/memory-game.js
@@ -1,4 +1,15 @@
 ﻿let gameCards = document.querySelectorAll("img.game-card");
+
+// willekeurig kaarten leggen
+for (let i = 0; i < gameCards.length; i++) {
+    let gameCard = gameCards[i];
+    let willekeurigGetal = Math.random() * 3; // willekeurig getal in interval [0,3[
+    willekeurigGetal = willekeurigGetal + 1; // willekeurig getal in interval [1, 4[
+    willekeurigGetal = Math.round(willekeurigGetal); // willekeurig getal in [1,2,3,4]
+    gameCard.src = "img/card_0" + willekeurigGetal + ".png";
+}
+
+// luisteren naar de kaarten :)
 for (let i = 0; i < gameCards.length; i++) {
     gameCards[i].addEventListener("click", function (e) {
         if (e.target.className.indexOf("game-card-down") >= 0) {
