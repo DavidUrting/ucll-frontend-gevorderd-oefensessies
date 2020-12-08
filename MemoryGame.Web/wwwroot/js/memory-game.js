ec91e91d3db99d072376c9dd6633f54d82e9f294
David Urting <david.urting@live.com>
2020-12-08 22:34:56 +0100

    omdraaien van de kaart (maar nog zonder terug omdraaien)

--- MemoryGame.Web/wwwroot/js/memory-game.js
+++ MemoryGame.Web/wwwroot/js/memory-game.js
@@ -1,1 +1,6 @@
-﻿
+﻿let gameCards = document.querySelectorAll("img.game-card");
+for (let i = 0; i < gameCards.length; i++) {
+    gameCards[i].addEventListener("click", function (e) {
+        e.target.className = "game-card";
+    });
+}
