a5d074f393d0054793c0416bc70c6c59579e62bb
David Urting <david.urting@live.com>
2020-12-08 22:38:55 +0100

    toggle van de card (dus omdraaien en dan weer omdraaien).

--- MemoryGame.Web/wwwroot/js/memory-game.js
+++ MemoryGame.Web/wwwroot/js/memory-game.js
@@ -1,6 +1,10 @@
 ﻿let gameCards = document.querySelectorAll("img.game-card");
 for (let i = 0; i < gameCards.length; i++) {
     gameCards[i].addEventListener("click", function (e) {
-        e.target.className = "game-card";
+        if (e.target.className.indexOf("game-card-down") >= 0) {
+            e.target.className = e.target.className.replace("game-card-down", "");
+        } else {
+            e.target.className += "game-card-down";
+        }        
     });
 }
